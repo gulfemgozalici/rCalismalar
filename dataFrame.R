9c3a66fbdf207bf89e5a5fa0040da1235392ef1f
Gülfem Gözalıcı <162334353+gulfemgozalici@users.noreply.github.com>
2025-11-23 18:45:39 +0300

    Add files via upload

---
+++ dataFrame.R
@@ -0,0 +1,13 @@
+# Farklı veri tiplerindeki dizileri birleştirir
+# fakat matrislerdeki veri tipleri, orijinal veri tipleri olarak kalır;
+# verilerin veri tipleri değişmez.
+
+karakterVeriTipliArray <- c("Merhaba", "Dunya", "Ben", "Insanim")
+sayisalVeriTipliArry <- c(12, 13, 45, 46)
+mantiksalVeriTipliArray <- c(T, F, F, T)
+
+birlestir = cbind(karakterVeriTipliArray, sayisalVeriTipliArry, mantiksalVeriTipliArray)
+birlestir
+
+benimFrame = data.frame(birlestir)
+benimFrame
